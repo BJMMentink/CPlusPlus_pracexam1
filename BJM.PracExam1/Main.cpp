ededd61e22d37688903119e80c5b7e253f457420
Benjamin Mentink <300079207@fvtc.edu>
2023-02-14 11:07:24 -0600

    Extra credit added

--- BJM.PracExam1/Main.cpp
+++ BJM.PracExam1/Main.cpp
@@ -1,19 +1,22 @@
+// Written by Ben Mentink
+// Extra Credit done
+
 #include <iostream>
 #include <conio.h>
 
 using namespace std;
 
-float Square(float num1);
-float Cube(float num1);
+float Square(float num1, int OP);
+float Cube(float num1, int OP);
 
 int main()
 {
 	float num1 = 0;
 	float num2 = 0;
-	int Operator = 0;
 	char Restart = 'Y';
 	while (Restart == 'Y' || Restart == 'y')
 	{
+		int Operator = 0;
 		cout << "Welcome to the basic calculator - App Developed by Ben Mentink\n INSTRUCTIONS\n";
 		cout << "Input a number folowed by the enter key then input the oportator followed by the enter key\n";
 		cout << "This app accepts Squaring (2) or Cubing (3)\n";
@@ -26,11 +29,11 @@ int main()
 		}
 		if (Operator == 2)
 		{
-			cout << Square(num1);
+			cout << "your answer is: " << Square(num1, 2);
 		}
 		else if (Operator == 3)
 		{
-			cout << Cube(num1);
+			cout << "your answer is: " << Cube(num1, 3);
 		}
 		cout << "\nWould you like to input another number? (Y)es or (N)o\n";
 		cin >> Restart;
@@ -38,11 +41,13 @@ int main()
 	(void)_getch();
 	return 0;
 }
-float Square(float num1)
+float Square(float num1, int OP)
 {
-	return(num1 * num1);
+	if (OP == 1)return num1;
+	else return num1 * Square(num1, OP - 1);
 }
-float Cube(float num1)
+float Cube(float num1, int OP)
 {
-	return(num1 * num1 * num1);
+	if (OP == 1) return num1;
+	else return num1 * Cube(num1, OP - 1);
 }
